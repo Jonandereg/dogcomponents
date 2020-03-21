35e2f9fa493336be00eaef4911f41a810954e0ea
jportella93 <jportella93@gmail.com>
2020-03-21 11:57:23 +0100

    feat: web components

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 import { getDogCollection } from './dogApi.js';
+import './dog-card.js';
+import './app-banner.js';
 
 function renderDogs(dogCollection) {
   Object.values(dogCollection).forEach(dogData => {
-    const dogCard = document.createElement('h2')
-    dogCard.textContent = dogData.breed
+    const dogCard = document.createElement('dog-card')
+    dogCard.breed = dogData.breed;
+    dogCard.img = dogData.picture;
     const dogsWrapper = document.querySelector('#dogsWrapper');
     dogsWrapper.appendChild(dogCard)
   });
@@ -11,5 +14,6 @@ function renderDogs(dogCollection) {
 
 window.addEventListener('load', async () => {
   const dogCollection = await getDogCollection(5);
+  console.log('---->: dogCollection', dogCollection)
   renderDogs(dogCollection);
 })
